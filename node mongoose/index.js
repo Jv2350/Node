80fdef49566ba3279a1df3f64487a45c06f04c5b
Jv2350 <tp8375735@gmail.com>
2024-04-13 23:50:27 +0530

    new

--- node mongoose/index.js
+++ node mongoose/index.js
@@ -1,11 +1,21 @@
-
 const express = require("express");
 const server = express();
 const morgan = require("morgan");
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 const productRouter = require('./routes/product')
 const userRouter = require("./routes/user")
 
-// server.use(morgan('dev')) //morgan loger
+// db connection
+main().catch(err => console.log(err));
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/ecommerce');
+    console.log("Database connected");
+}
+
+
+
+
 // parsers
 server.use(express.json());// middleware parser for the body
 server.use(morgan('default'));
