27d6d0713cd90f281ec33f8bd80915c28468862a
Jv2350 <tp8375735@gmail.com>
2024-04-17 14:00:03 +0530

    update

--- node mongoose/controller/product.js
+++ node mongoose/controller/product.js
@@ -6,58 +6,58 @@ const Product = model.Product;
 // Create
 exports.createProduct = (req, res) => {
   const product = new Product(req.body);
-  product.save((err,doc)=>{
-    console.log({err,doc})
-    if(err){
+  product.save((err, doc) => {
+    console.log({ err, doc })
+    if (err) {
       res.status(400).json(err);
-    } else{
+    } else {
       res.status(201).json(doc);
     }
   })
 
-  
+
 };
 
 exports.getAllProducts = async (req, res) => {
-  const products = await Product.find({price:{$gt:500}});
+  const products = await Product.find();
   res.json(products);
 };
 
 exports.getProduct = async (req, res) => {
   const id = req.params.id;
-  console.log({id})
+  console.log({ id })
   const product = await Product.findById(id);
   res.json(product);
 };
 exports.replaceProduct = async (req, res) => {
   const id = req.params.id;
-  try{
-  const doc = await Product.findOneAndReplace({_id:id},req.body,{new:true})
-  res.status(201).json(doc);
+  try {
+    const doc = await Product.findOneAndReplace({ _id: id }, req.body, { new: true })
+    res.status(201).json(doc);
   }
-  catch(err){
+  catch (err) {
     console.log(err);
     res.status(400).json(err);
   }
 };
 exports.updateProduct = async (req, res) => {
   const id = req.params.id;
-  try{
-  const doc = await Product.findOneAndUpdate({_id:id},req.body,{new:true})
-  res.status(201).json(doc);
+  try {
+    const doc = await Product.findOneAndUpdate({ _id: id }, req.body, { new: true })
+    res.status(201).json(doc);
   }
-  catch(err){
+  catch (err) {
     console.log(err);
     res.status(400).json(err);
   }
 };
 exports.deleteProduct = async (req, res) => {
   const id = req.params.id;
-  try{
-  const doc = await Product.findOneAndDelete({_id:id})
-  res.status(201).json(doc);
+  try {
+    const doc = await Product.findOneAndDelete({ _id: id })
+    res.status(201).json(doc);
   }
-  catch(err){
+  catch (err) {
     console.log(err);
     res.status(400).json(err);
   }
